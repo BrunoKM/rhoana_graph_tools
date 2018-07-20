ebd1f9baec133c0c8682a0d9f9907b98be35cfd2
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2018-07-20 12:14:54 -0400

    Add function random_weighted_with_min_motif_freq

--- generate_graphs.py
+++ generate_graphs.py
@@ -15,6 +15,82 @@ def random_with_min_motif_freq(motif, min_freq, num_nodes, connectivity=None, sh
     :param shuffle_on_connecting:
     :return:
     """
+    # Randomly allocate the motifs within the adjacency matrix, keeping track of where they have been allocated in mask
+    adj_matrix, mask = rand_allocate_motifs(motif, min_freq, num_nodes, shuffle_on_connecting, dtype=np.int8)
+
+    # Fill in the values not filled in by the motifs with random connections with connectivity p
+    if connectivity is None:
+        # If connectivity not specified, use the average connectivity of the parts filled in with the motifs
+        p = np.sum(adj_matrix * mask) / np.sum(mask)
+    else:
+        # Otherwise use the value given
+        p = connectivity
+    rand_connection_fill = np.random.binomial(1, p, size=adj_matrix.shape)
+    # Zero-out the entries that coincide with the motifs assigned earlier
+    mask_inverse = np.ones(adj_matrix.shape, dtype=np.int8) - mask
+    rand_connection_fill *= mask_inverse
+    # Finally combine the motif entries and the randomly generated connections
+    adj_matrix += rand_connection_fill
+    return adj_matrix
+
+
+def random_weighted_with_min_motif_freq(motif, min_freq, num_nodes, motif_edge_std=0, rand_edge_mean=1,
+                                        rand_edge_std=0, connectivity=None, shuffle_on_connecting=True, discrete=False):
+    """
+    Generate a random weighted directed graph with specified minimum frequency of occurrences of a certain motif.
+    :param motif:
+    :param min_freq:
+    :param num_nodes:
+    :param connectivity:
+    :param shuffle_on_connecting:
+    :return:
+    """
+    # Randomly allocate the motifs within the adjacency matrix, keeping track of where they have been allocated in mask
+    adj_matrix, mask = rand_allocate_motifs(motif, min_freq, num_nodes, shuffle_on_connecting, dtype=np.float32)
+
+    # Generate the deviation for all edges
+    # Generate only once for both motif edges and the rest of random edges to improve efficiency (as these two cases
+    # are not overlapping)
+    edge_var = np.random.normal(loc=0.0, scale=1.0, size=adj_matrix.shape)
+
+    # Add variation to the motif edge weights
+    if motif_edge_std != 0:
+        motif_edge_var = edge_var * motif_edge_std
+        # Want to add deviation only to existing edges todo: consider adding possibility of altering non-existing edges
+        edge_var_mask = (adj_matrix != 0)
+        adj_matrix += motif_edge_var * edge_var_mask
+
+    # Fill in the values not filled in by the motifs with random connections with connectivity p
+    if connectivity is None:
+        # If connectivity not specified, use the average connectivity of the parts filled in with the motifs
+        p = np.sum(adj_matrix * mask) / np.sum(mask)
+    else:
+        # Otherwise use the value given
+        p = connectivity
+    rand_connection_fill = np.random.binomial(1, p, size=adj_matrix.shape)
+    # Zero-out the entries that coincide with the motifs assigned earlier
+    mask_inverse = np.ones(adj_matrix.shape, dtype=np.int32) - mask
+    rand_connection_fill *= mask_inverse
+    if rand_edge_std != 0:
+        # Get the random weights for the connected edges
+        rand_connection_weights = (edge_var * rand_edge_std) + rand_edge_mean
+        rand_connection_weights *= rand_connection_fill
+    else:
+        rand_connection_weights = rand_connection_fill
+
+    adj_matrix += rand_connection_weights
+    return adj_matrix
+
+
+def rand_allocate_motifs(motif, min_freq, num_nodes, shuffle_on_connecting, dtype=np.float64):
+    """
+    Handles the random allocation of motifs for the random_with_min_motif_freq and
+    random_weighted_with_min_motif_freq function.
+    :param dtype: Specifies the dtype of the adjacency matrix. For a normal directed graph bool or int will do. For
+    a graph with weighted edges with weights taking on real values, or if motifs are to follow a random distribution,
+    use float.
+    :return: tuple of (the adjacency matrix with assigned motifs, the edge positions where motifs have been assigned
+    """
     motif_size = motif.shape[0]
     # Assert min_freq is below the maximum minimum frequency this algorithm is applicable for
     max_freq_allowed = num_nodes - motif_size + 1
@@ -22,7 +98,7 @@ def random_with_min_motif_freq(motif, min_freq, num_nodes, connectivity=None, sh
         raise AttributeError(f'min_frequency {min_freq} too large. Maximum allowed for number of nodes {num_nodes}'
                              f' is {num_nodes - motif_size + 1}.')
 
-    adj_matrix = np.zeros([num_nodes, num_nodes])
+    adj_matrix = np.zeros([num_nodes, num_nodes], dtype=dtype)
     # Create a mask to mark where the added motifs are in the overall network
     mask = np.zeros(adj_matrix.shape, dtype=np.bool)
 
@@ -69,21 +145,7 @@ def random_with_min_motif_freq(motif, min_freq, num_nodes, connectivity=None, sh
         else:
             last_motif_mat = motif
         last_idx = i
-
-    # Fill in the values not filled in by the motifs with random connections with connectivity p
-    if connectivity is None:
-        # If connectivity not specified, use the average connectivity of the parts filled in with the motifs
-        p = np.sum(adj_matrix * mask) / np.sum(mask)
-    else:
-        # Otherwise use the value given
-        p = connectivity
-    rand_connection_fill = np.random.binomial(1, p, size=adj_matrix.shape)
-    # Zero-out the entries that coincide with the motifs assigned earlier
-    mask_inverse = np.ones(adj_matrix.shape, dtype=np.int32) - mask
-    rand_connection_fill *= mask_inverse
-    # Finally combine the motif entries and the randomly generated connections
-    adj_matrix += rand_connection_fill
-    return adj_matrix
+    return adj_matrix, mask
 
 
 def get_random_indices(num_indices, num_nodes, motif_size):
@@ -127,6 +189,6 @@ if __name__ == '__main__':
     print(g)
     for size in [1000, 3000, 5000, 8000, 10000]:
         start_time = time.time()
-        g = random_with_min_motif_freq(motif, 100, size)
+        g = random_with_min_motif_freq(motif, int(size/2), size, shuffle_on_connecting=False)
         print(f"Size: {size}, Time taken: {time.time() - start_time:.2f}s")
         del g
