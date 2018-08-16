90a67d16ca78f3cb71e6ba79f2a221e1e14b3ab3
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2018-08-16 11:28:17 -0400

    Added pre-loading of dataset into memory functionality

--- embedding/dataset_util.py
+++ embedding/dataset_util.py
@@ -10,17 +10,29 @@ from torch.utils.data import DataLoader, Dataset
 class GEDDataset(Dataset):
     """Graph Edit Distance dataset class."""
 
-    def __init__(self, h5_file, which_set='train', adj_dtype=np.float32, transform=None):
+    def __init__(self, h5_file, which_set='train', adj_dtype=np.float32, transform=None, load_into_memory=True):
         # todo: finish docstring
         """
 
         """
-        # todo: Implement a different fileformat/loading method as h5py does not allow for multiple workers
         self.h5_file = h5py.File(h5_file, mode="r")
         assert which_set.lower() in ['train', 'val', 'test']
         self.which_set = which_set.lower()
         self.adj_dtype = adj_dtype
         self.transform = transform
+        self.load_into_memory = load_into_memory
+        if load_into_memory:
+            # Load the entire dataset features into memory
+            self.graph1_set = self.h5_file[self.which_set + "_graph1"][()]
+            self.graph2_set = self.h5_file[self.which_set + "_graph2"][()]
+            self.labels_set = self.h5_file[self.which_set + "_labels"][()]
+            print('Data loaded into memory.')
+        else:
+            # Just load pointers to the dataset, while dataset remains on disk
+            self.graph1_set = self.h5_file[self.which_set + "_graph1"]
+            self.graph2_set = self.h5_file[self.which_set + "_graph2"]
+            self.labels_set = self.h5_file[self.which_set + "_labels"]
+            print('Dataset being accessed from disc (single worker only).')
 
     def __del__(self):
         if hasattr(self, 'h5_file'):
@@ -30,9 +42,9 @@ class GEDDataset(Dataset):
         return len(self.h5_file[self.which_set + '_labels'])
 
     def __getitem__(self, idx):
-        graph1 = self.h5_file[self.which_set + "_graph1"][idx]
-        graph2 = self.h5_file[self.which_set + "_graph2"][idx]
-        label = self.h5_file[self.which_set + "_labels"][idx]
+        graph1 = self.graph1_set[idx]
+        graph2 = self.graph2_set[idx]
+        label = self.labels_set[idx]
 
         graph1 = graph1.astype(self.adj_dtype)
         graph2 = graph2.astype(self.adj_dtype)
