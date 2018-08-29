4dc81d4b2a17d197630bef83656d585e641220b1
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2018-08-29 10:14:12 +0100

    asdf

--- embedding/embedding_models.py
+++ embedding/embedding_models.py
@@ -6,12 +6,21 @@ from layers import GraphConvolution
 
 
 class GCN(nn.Module):
-    def __init__(self, num_features, embed_size, dropout=0.):
+    def __init__(self, num_features, embed_size, hidden=[100], dropout=0.):
         super(GCN, self).__init__()
-        num_hidden = 100
-        self.gc1 = GraphConvolution(num_features, num_hidden)
-        self.gc2 = GraphConvolution(num_hidden, embed_size)
         self.dropout = dropout
+
+        self.layer_sizes = [num_features] + hidden
+        self.layers = []
+        # Add all the hidden layers and activation functions
+        for i in range(len(self.layer_sizes) - 1):
+            self.layers.append(GraphConvolution(self.layer_sizes[i], self.layer_sizes[i + 1]))
+            self.layers.append(F.relu(inplace=True))
+        # Add the last (embedding) layer with no activation function
+        self.layers.append(GraphConvolution(hidden[-1], embed_size))
+
+        # Create the model using nn.Sequential
+        self.layers = nn.ModuleList(*layers)
 
     def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
